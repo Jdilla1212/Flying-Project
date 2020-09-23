1d8495c26a9df6fd33db1246fd6a4563b2d919f8
Josh Padilla <jdp237@nau.edu>
2020-09-22 19:13:57 -0700

    test

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
+$.ajax({
+    url: "https://api.astrocats.space/catalog?ra=21:23:32.16&dec=-53:01:36.08&radius=2",
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
 
-    $.ajax({
-        url: "https://api.astrocats.space/catalog?ra=21:23:32.16&dec=-53:01:36.08&radius=2",
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+let map, infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 6
     });
-
-    let map, infoWindow;
-
-    function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: {
-                lat: -34.397,
-                lng: 150.644
+    infoWindow = new google.maps.InfoWindow(); // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                console.log(pos);
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(map);
+                map.setCenter(pos);
             },
-            zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow(); // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                position => {
-                    const pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
-                    console.log(pos);
-                    infoWindow.setPosition(pos);
-                    infoWindow.setContent("Location found.");
-                    infoWindow.open(map);
-                    map.setCenter(pos);
-                },
-                () => {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                }
-            );
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-    }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-            browserHasGeolocation ?
-            "Error: The Geolocation service failed." :
-            "Error: Your browser doesn't support geolocation."
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
         );
-        infoWindow.open(map);
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation ?
+        "Error: The Geolocation service failed." :
+        "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+}
